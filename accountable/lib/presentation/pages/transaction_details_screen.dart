5d30e58d744e845f7e71fca92056a2df5ab8ac85
Smh1111 <mobilesoemoehtet11@gmail.com>
2025-04-09 11:24:34 +0700

    fix: small change

--- accountable/lib/presentation/pages/transaction_details_screen.dart
+++ accountable/lib/presentation/pages/transaction_details_screen.dart
@@ -1,9 +1,18 @@
 import 'package:accountable/presentation/pages/addTransaction.dart';
 import 'package:flutter/material.dart';
 
-class TransactionDetailScreen extends StatelessWidget {
+class TransactionDetailScreen extends StatefulWidget {
   const TransactionDetailScreen({super.key});
 
+  @override
+  State<TransactionDetailScreen> createState() => _TransactionDetailScreenState();
+}
+
+class _TransactionDetailScreenState extends State<TransactionDetailScreen> {
+  
+  
+  
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
